0c536eafd7ccbcd183911b5e62646872a2dd4166
hyeokkiyaa <khm38607574@gmail.com>
2024-12-18 01:40:59 +0900

    24-12-18

---
+++ src/24_12_18/Main.java
@@ -0,0 +1,15 @@
+class Solution {
+	public int solution(String t, String p) {
+		int answer = 0;
+		int len_p = p.length();
+		long long_p = Long.parseLong(p);
+		for (int i = 0; i <= t.length() - len_p; i++) {
+			String num = t.substring(i, i + len_p);
+			long long_num = Long.parseLong(num);
+			if (long_num <= long_p) {
+				answer++;
+			}
+		 }
+		return answer;
+	}
+}
